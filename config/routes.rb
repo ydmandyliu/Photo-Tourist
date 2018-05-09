886ba4c2b4e9452cc5d76e62d699abec52cb897a
Mandy Liu <ydmandyliu@gmail.com>
2018-05-09 15:16:18 -0700

    created resouces called Cities and States

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json } do
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
+    resources :cities, except: [:new, :edit]
+    resources :states, except: [:new, :edit]
   end
 
   get '/ui'  => 'ui#index'
--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -5,38 +5,38 @@ RSpec.describe "ApiDevelopments", type: :request do
   	JSON.parse(response.body)
   end
   describe "RDBMS-backed" do
-  	before(:each) { Foo.delete_all }
-  	after(:each) { Foo.delete_all }
+  	before(:each) { City.delete_all }
+  	after(:each) { City.delete_all }
 
   	it "create RDBMS-backed model" do
-  	  object=Foo.create(:name=>"test")
-  	  expect(Foo.find(object.id).name).to eq("test")
+  	  object=City.create(:name=>"Baltimore")
+  	  expect(City.find(object.id).name).to eq("Baltimore")
   	end
 
   	it "expose RDBMS-backed API resource" do
-      object=Foo.create(:name=>"test")
-      expect(foos_path).to eq("/api/foos")
-      get foo_path(object.id)
+      object=City.create(:name=>"Baltimore")
+      expect(cities_path).to eq("/api/cities")
+      get city_path(object.id)
       expect(response).to have_http_status(:ok)
-      expect(parsed_body["name"]).to eq("test")
+      expect(parsed_body["name"]).to eq("Baltimore")
   	end
   end
 
   describe "MongoDB-backed" do
-  	before(:each) { Bar.delete_all }
-  	after(:each) { Bar.delete_all }
+  	before(:each) { State.delete_all }
+  	after(:each) { State.delete_all }
 
   	it "create MongoDB-backed model" do
-      object=Bar.create(:name=>"test")
-      expect(Bar.find(object.id).name).to eq("test")
+      object=State.create(:name=>"Maryland")
+      expect(State.find(object.id).name).to eq("Maryland")
     end
 
   	it "expose MongoDB-backed API resource" do
-      object=Bar.create(:name=>"test")
-      expect(bars_path).to eq("/api/bars")
-      get bar_path(object.id)
+      object=State.create(:name=>"Maryland")
+      expect(states_path).to eq("/api/states")
+      get state_path(object.id)
       expect(response).to have_http_status(:ok)
-      expect(parsed_body["name"]).to eq("test")
+      expect(parsed_body["name"]).to eq("Maryland")
       expect(parsed_body).to include("created_at")
       expect(parsed_body).to include("id"=>object.id.to_s)
     end
