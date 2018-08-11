10ced1bbbf536780d95fbd49e91f4a178af7ba14
Mandy Liu <ydmandyliu@gmail.com>
2018-08-10 23:46:52 -0700

    implemented rule-based authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,21 +2,47 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   #make the connection between controller action and associated view
   include ActionController::ImplicitRender
+  include Pundit
+  
   before_action :configure_permitted_parameters, if: :devise.controller?
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from Mongoid::Errors::DocumentNotFound, with: :record_not_found
+  rescue_from ActionController::ParameterMissing, with: :missing_parameter
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
-  	def record_not_found(exception)
-  		payload = {
-  			errors: { full_messages:["cannot find id[#{params[:id]}]"] }
-  		}
-  		render :json=>payload, :status=>:not_found
-  		Rails.logger.debug exception.message
+    def full_message_error full_message, status
+      payload = {
+        errors: { full_messages:["#{full_message}"] }
+      }
+      render :json=>payload, :status=>status
+    end
 
-  		def configure_permitted_parameters
-  			devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  		end
+  	def record_not_found(exception)
+  		full_messages_error "cannot find id[#{params[:id]}]", :not_found
+      Rails.logger.debug exception.message
   	end
+
+    def missing_parameter(exception) 
+      payload = {
+        errors: { full_messages:["#{exception.message}"] }
+      }
+      render :json=>payload, :status=>:bad_request
+      Rails.logger.debug exception.message
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+
+    def user_not_authorized(exception)
+      user=pundit_user ? pundit_user.uid : "Anonymous user"
+      payload = {
+        errors: {full_messages:["#{user} not authorized to #{exception.query}"]}
+      }
+      render :json=>payload, :status=>:forbidden
+      Rails.logger.debug exception
+    end
+
 end
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,21 +2,29 @@ class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :update, :destroy]
   wrap_parameters :image, include: ["caption"]
   before_action :authenticate_user!, only: [:create, :update, :destroy]
+  after_action :verify_authorized
+  after_action :verify_policy_scoped, only: [:index]
 
   # GET /images
   # GET /images.json
   def index
-    @images = Image.all
+    authorize Image
+    @images = policy_scope(Image.all);
+    @images = ImagePolicy.merge(@images)
   end
 
   # GET /images/1
   # GET /images/1.json
   def show
+    authorize @image
+    images = policy_scope(Image.where(:id=>@image.id))
+    @image = ImagePolicy.merge(images).first
   end
 
   # POST /images
   # POST /images.json
   def create
+    authorize Image
     @image = Image.new(image_params)
     @image.creator_id = current_user.id
 
