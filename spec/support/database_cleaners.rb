16d686a5e717f1fbe51653cbaa014bada89d1b17
Mandy Liu <ydmandyliu@gmail.com>
2018-09-12 23:45:46 -0700

    added geocode cache

--- spec/support/database_cleaners.rb
+++ spec/support/database_cleaners.rb
@@ -2,15 +2,15 @@ require 'database_cleaner'
 
 shared_context "db_cleanup" do |ar_strategy=:truncation|
   before(:all) do
-    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:mongoid].strategy = :truncation, {:except => %w(locations)}
     DatabaseCleaner[:active_record].strategy = ar_strategy
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   after(:each, :type=>feature) do
     Capybara.reset_sessions!  #cleared up some random failures
   end
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
 end
 
@@ -25,7 +25,7 @@ end
 
 shared_context "db_clean_after" do
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
 end
 
@@ -40,12 +40,12 @@ end
 
 shared_context "db_cleanup_each" do |ar_strategy=:truncation|
   before(:all) do
-    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:mongoid].strategy = :truncation, {:except => %w(locations)}
     DatabaseCleaner[:active_record].strategy = ar_strategy
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   before(:each) do
     DatabaseCleaner.start
