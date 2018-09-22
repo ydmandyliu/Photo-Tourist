ebca48c47ea291c1e81c85b114752dcfb350c047
Mandy Liu <ydmandyliu@gmail.com>
2018-09-22 01:45:31 -0700

    modified image content factory

--- spec/factories/image_contents.rb
+++ spec/factories/image_contents.rb
@@ -1,7 +1,14 @@
+require_relative '../support/image_content_helper.rb'
 FactoryGirl.define do
   factory :image_content do
     content_type "image/jpg"
         #default to the original size when we generate just attributes
-    content { File.open("db/images/sample.jpg","rb") {|f| Base64.encode64(f.read) } }
+    sequence(:content) {|idx|
+      File.open(ImageContentHelper.sample_filepath,"rb") { |f| 
+        image=StringIO.new(f.read)
+        image=ImageContentCreator.annotate(idx, image)
+        Base64.encode64(image)
+      }
+    }
   end
 end
