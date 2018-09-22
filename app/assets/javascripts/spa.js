06c6b490c640bd0a2ee1c88bc232ddf41fc85b4d
Mandy Liu <ydmandyliu@gmail.com>
2018-09-22 01:55:51 -0700

    added new modules

--- app/assets/javascripts/spa.js
+++ app/assets/javascripts/spa.js
@@ -28,8 +28,19 @@
 //= require spa/authz/authz.service
 //= require spa/authz/base_policy.service
 
+//= require spa/geoloc/geoloc.module
+//= require spa/geoloc/geocoder.service
+//= require spa/geoloc/my_location.service
+// require spa/geoloc/jhu_location.config
+//= require spa/geoloc/current_origin/current_origin.service
+//= require spa/geoloc/current_origin/current_origin_selector.component
+
 //= require spa/layout/layout.module
+//= require spa/layout/image_query_size.service
 //= require spa/layout/navbar/navbar.component
+//= require spa/layout/image_loader/data_utils.service
+//= require spa/layout/image_loader/image_loader.component
+//= require spa/layout/image_viewer/image_viewer.component
 
 //= require spa/states/states.module
 //= require spa/states/states.service
