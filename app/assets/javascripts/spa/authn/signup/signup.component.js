ce54dba7289094b8678747b3f32c1e5e4b4d6de1
Mandy Liu <ydmandyliu@gmail.com>
2018-12-05 20:43:08 -0800

    commented out the output lines for development purpose

--- app/assets/javascripts/spa/authn/signup/signup.component.js
+++ app/assets/javascripts/spa/authn/signup/signup.component.js
@@ -19,24 +19,25 @@
     var vm=this;
     vm.signupForm = {}
     vm.signup = signup;
+    vm.getCurrentUser = Authn.getCurrentUser;
 
     vm.$onInit = function() {
-      console.log("SignupController",$scope);
+      // console.log("SignupController",$scope);
     }
     return;
     //////////////
     function signup() {
-      console.log("signup...");
+      // console.log("signup...");
       $scope.signup_form.$setPristine();
       Authn.signup(vm.signupForm).then(
         function(response){
           vm.id = response.data.data.id;
-          console.log("signup complete", response.data, vm);          
+          // console.log("signup complete", response.data, vm);          
           $state.go("home");
         },
         function(response){
           vm.signupForm["errors"]=response.data.errors;
-          console.log("signup failure", response, vm);          
+          // console.log("signup failure", response, vm);          
         }
       );
     }
