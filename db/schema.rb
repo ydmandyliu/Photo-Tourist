a87cfb0358baf8d83b154ea627e1de5de5f64512
Mandy Liu <ydmandyliu@gmail.com>
2018-12-06 00:13:51 -0800

    updated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,6 @@ ActiveRecord::Schema.define(version: 20180910000723) do
     t.string   "encrypted_password",     default: "",      null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
-    t.boolean  "allow_password_change",  default: false
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",          default: 0,       null: false
     t.datetime "current_sign_in_at"
@@ -97,7 +96,7 @@ ActiveRecord::Schema.define(version: 20180910000723) do
     t.datetime "updated_at",                               null: false
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  # add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
